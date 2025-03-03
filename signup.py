31b30131ed60c7204e238ee15dc8b7267409a191
vawnz <eimanjunel27@gmail.com>
2025-03-04 06:09:38 +0800

    Update signup.py

--- signup.py
+++ signup.py
@@ -5,24 +5,26 @@ def signup():
     checkPass = input("Enter again your Password: ")
 
     if newPass != checkPass:
-        print("Passwords does not match. Try again.")
+        print("Passwords do not match. Try again.")
         signup()
     else:
-        choice = input("Account created successfully! Proceed to Log-In? (y/n): ")
+        choice = input("\nAccount created successfully! Proceed to Log-In? (y/n): ")
         if choice == 'y':
             print("\nWelcome back! Please enter your Account")
-            username = input("Please enter your Username: ")
-            password = input("Please enter your Password: ")
-            
             tries = 5
+            loggedIn = False
             while tries > 0:
-                if username != newUser and password != newPass:
+                username = input("Please enter your Username: ")
+                password = input("Please enter your Password: ")
+                if username == newUser and password == newPass:
+                    print(f"\nHello again, {username}! You are now Logged In.")
+                    loggedIn = True
+                    break
+                else:
                     tries -= 1
-                print(f"Invalid credentials. Please Try Again. {tries} attempt/s left.")
-            else:
-                print(f"Hello again, {username}! You are now Logged In.")
-                if tries == 0:
-                    print("You ran out of attempts. Try again later.")
+                    print(f"Invalid credentials. {tries} attempt/s left.\n")
+            if not loggedIn:
+                print("\nYou ran out of attempts. Try again later.")
         elif choice == 'n':
             print("See you around!")
         else:
